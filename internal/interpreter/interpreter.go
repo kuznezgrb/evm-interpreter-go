608df8501ea6a12d0255ba9c36b6dbbaefefcb98
kuznez <kuznez_grb@mail.ru>
2026-09-02 08:27:19 +0300

    Add PUSH0-PUSH32 and DUP3-DUP16 opcode support
    Generalizes push1/dup1/dup2 into parameterized handlers registered
    in a loop, covering the full PUSH and DUP opcode ranges.

--- internal/interpreter/interpreter.go
+++ internal/interpreter/interpreter.go
@@ -11,13 +11,59 @@ type Opcode byte
 type Operation func() error
 
 const (
-	STOP  Opcode = 0x00
-	PUSH1 Opcode = 0x60
+	STOP   Opcode = 0x00
+	PUSH0  Opcode = 0x5F
+	PUSH1  Opcode = 0x60
+	PUSH2  Opcode = 0x61
+	PUSH3  Opcode = 0x62
+	PUSH4  Opcode = 0x63
+	PUSH5  Opcode = 0x64
+	PUSH6  Opcode = 0x65
+	PUSH7  Opcode = 0x66
+	PUSH8  Opcode = 0x67
+	PUSH9  Opcode = 0x68
+	PUSH10 Opcode = 0x69
+	PUSH11 Opcode = 0x6A
+	PUSH12 Opcode = 0x6B
+	PUSH13 Opcode = 0x6C
+	PUSH14 Opcode = 0x6D
+	PUSH15 Opcode = 0x6E
+	PUSH16 Opcode = 0x6F
+	PUSH17 Opcode = 0x70
+	PUSH18 Opcode = 0x71
+	PUSH19 Opcode = 0x72
+	PUSH20 Opcode = 0x73
+	PUSH21 Opcode = 0x74
+	PUSH22 Opcode = 0x75
+	PUSH23 Opcode = 0x76
+	PUSH24 Opcode = 0x77
+	PUSH25 Opcode = 0x78
+	PUSH26 Opcode = 0x79
+	PUSH27 Opcode = 0x7A
+	PUSH28 Opcode = 0x7B
+	PUSH29 Opcode = 0x7C
+	PUSH30 Opcode = 0x7D
+	PUSH31 Opcode = 0x7E
+	PUSH32 Opcode = 0x7F
 
 	POP Opcode = 0x50
 
-	DUP1 Opcode = 0x80
-	DUP2 Opcode = 0x81
+	DUP1  Opcode = 0x80
+	DUP2  Opcode = 0x81
+	DUP3  Opcode = 0x82
+	DUP4  Opcode = 0x83
+	DUP5  Opcode = 0x84
+	DUP6  Opcode = 0x85
+	DUP7  Opcode = 0x86
+	DUP8  Opcode = 0x87
+	DUP9  Opcode = 0x88
+	DUP10 Opcode = 0x89
+	DUP11 Opcode = 0x8A
+	DUP12 Opcode = 0x8B
+	DUP13 Opcode = 0x8C
+	DUP14 Opcode = 0x8D
+	DUP15 Opcode = 0x8E
+	DUP16 Opcode = 0x8F
 
 	SWAP1  Opcode = 0x90
 	SWAP2  Opcode = 0x91
@@ -56,10 +102,22 @@ func NewInterpreter(stack *stack.Stack, code []byte) *Interpreter {
 
 	operations := map[Opcode]Operation{
 		STOP:  inter.stop,
-		PUSH1: inter.push1,
+		PUSH0: inter.push0,
 		POP:   inter.pop,
-		DUP1:  inter.dup1,
-		DUP2:  inter.dup2,
+	}
+
+	for op := PUSH1; op <= PUSH32; op++ {
+		n := int(op - PUSH1 + 1)
+		operations[op] = func() error {
+			return inter.push(n)
+		}
+	}
+
+	for op := DUP1; op <= DUP16; op++ {
+		n := int(op - DUP1)
+		operations[op] = func() error {
+			return inter.dup(n)
+		}
 	}
 
 	for op := SWAP1; op <= SWAP16; op++ {
@@ -96,14 +154,22 @@ func (i *Interpreter) stop() error {
 	return nil
 }
 
-func (i *Interpreter) push1() error {
-	if i.pc+1 >= i.lenCode {
+func (i *Interpreter) push0() error {
+	val := new(big.Int).SetBytes([]byte{0x00})
+	i.stack.Push(val)
+	i.pc++
+
+	return nil
+}
+
+func (i *Interpreter) push(s int) error {
+	if i.pc+s+1 > i.lenCode {
 		return fmt.Errorf("no value to add to stack")
 	}
 
-	val := new(big.Int).SetBytes(i.code[i.pc+1 : i.pc+2])
+	val := new(big.Int).SetBytes(i.code[i.pc+1 : i.pc+s+1])
 	i.stack.Push(val)
-	i.pc += 2
+	i.pc += s + 1
 
 	return nil
 }
@@ -117,22 +183,12 @@ func (i *Interpreter) pop() error {
 	return nil
 }
 
-func (i *Interpreter) dup1() error {
+func (i *Interpreter) dup(s int) error {
 	defer func() {
 		i.pc++
 	}()
 
-	val := i.stack.PeekN(0)
-	i.stack.Push(val)
-	return nil
-}
-
-func (i *Interpreter) dup2() error {
-	defer func() {
-		i.pc++
-	}()
-
-	val := i.stack.PeekN(1)
+	val := i.stack.PeekN(s)
 	i.stack.Push(val)
 	return nil
 }
